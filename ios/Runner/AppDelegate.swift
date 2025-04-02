927eaec283e1ac9165655d4deda5542d0efde039
zhongguoxu <zhongguo@ualberta.ca>
2025-04-02 10:41:41 -0600

    add google api key
    add google api key to android, ios and web

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAxZOV5TpE9YjJh77040FSbTyGmtZqZtWU")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
